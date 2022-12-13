0e36407fb7b1ba3fe2871c4e24a1138658b8bf64
Aída Muñoz Monjas <amunoza225@gmail.com>
2022-12-13 09:15:12 +0100

    esto no hace falta

--- practica-LOINC/code/borra/pca.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-from sklearn.decomposition import PCA
-
-def pca(x):
-   pca=PCA(n_components='mle',svd_solver='full')
-   #pca=PCA(n_components=12,svd_solver='full')
-   return pca.fit_transform(x)
